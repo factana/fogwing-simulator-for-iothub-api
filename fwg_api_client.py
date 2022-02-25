2d9106af6b3c75d8795be7aae7e2ab3ffc2a575a
Ajay <71346771+Ajaykumar-Kanojiya@users.noreply.github.com>
2022-02-25 16:57:30 +0530

    update the endpoint utrl

--- fwg_api_client.py
+++ fwg_api_client.py
@@ -9,13 +9,13 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 def send_data_API(user_name, password, sample_data):
     # URL link for getting API-KEY.
     credentials = {"username": user_name, "password": password}
-    api_key_url = 'https://enterprise.fogwing.net/api/iothub/getApiKey'
+    api_key_url = 'https://enterprise.fogwing.net/api/iothub/getApiToken'
     resp = requests.post(api_key_url, json=credentials, verify=False)
     resp_dict = json.loads(resp.text)
     api_key = resp_dict['token']
 
     # URL link for posting data over Fogwing IoTHub.
-    header = {"API-KEY": api_key}
+    header = {"API-TOKEN": api_key}
     payload_url = 'https://enterprise.fogwing.net/api/iothub/postPayload'
     payload_resp = requests.post(payload_url, data=json.dumps(sample_data), headers=header, verify=False)
     print(json.dumps(sample_data, indent=4))
@@ -26,14 +26,9 @@ if __name__ == '__main__':
     # Reading configuration file.
     with open('configuration.json', 'r') as file:
         config = json.load(file)
-
     # Fogwing IoTHub credentials
     usr_name = config['Fwg_IoTHub_cred']['username']
     psw = config['Fwg_IoTHub_cred']['password']
-
     # Sample data to send over Fogwing IoTHub.
     data = config['payload']
-
     send_data_API(usr_name, psw, data)
-# "pub_topic": "Enter your Fogwing IoTHub publish topic",
-# Enter your Fogwing IoTHub password
